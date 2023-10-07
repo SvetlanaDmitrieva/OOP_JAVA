efa347f7288b5fbb8eaa81a136fb663eadd86792
Dmitrieva Svetlana <sv-dmitrieva@list.ru>
2023-10-07 05:31:25 +0300

    01 HW commit-02

--- HW-01/ru/geekbrains/HWork01.java
+++ HW-01/ru/geekbrains/HWork01.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class HWork01 {
     public static void main(String[] args) {
-
+        // созданы две категории товаров, добавлено по 3 товара в кажд. категорию
         Category category1 = new Category("Category 1");
         category1.addProducts(new Product("Product 1", 10.99, 4.5));
         category1.addProducts(new Product("Product 2", 5.99, 3.5));
@@ -17,18 +17,18 @@ public class HWork01 {
 
         Category[] Categorys = new Category[] { category1, category2 };
         System.out.println(" Товары до покупки");
-        for (Category category : Categorys) {
-            System.out.println(category.getName());
+        for (Category category : Categorys) { // вывод на печать всех имеющихся товароа
+            System.out.println(category.getName()); // по категориям
             ArrayList<Product> products = category.getProducts();
             for (Product product : products) {
                 System.out.println(product);
             }
         }
-        User user1 = new User("user1", "password1");
-        user1.getBasket().addProducts(category1.getProducts().get(0));
-        user1.getBasket().addProducts(category2.getProducts().get(0));
+        User user1 = new User("user1", "password1"); // создано 2 пользователя
+        user1.getBasket().addProducts(category1.getProducts().get(0)); // внесено по 2 товара
+        user1.getBasket().addProducts(category2.getProducts().get(0)); // в корзины каждому пользователю
 
-        category1.getProducts().remove(0);
+        category1.getProducts().remove(0); // удалены из магазина товары, внесенные в корзины
         category2.getProducts().remove(0);
 
         User user2 = new User("user2", "password2");
@@ -40,7 +40,7 @@ public class HWork01 {
 
         User[] Users = new User[] { user1, user2 };
 
-        for (User user : Users) {
+        for (User user : Users) { // вывод на печать содержимое корзин пользователей
             System.out.println(user.getLogin());
             ArrayList<Product> userProducts = user.getBasket().getProducts();
             for (Product product : userProducts) {
@@ -49,7 +49,7 @@ public class HWork01 {
         }
 
         System.out.println(" Товары после покупки(остаток)");
-        for (Category category : Categorys) {
+        for (Category category : Categorys) { // вывод на печать всех оставшихся товаров
             System.out.println(category.getName());
             ArrayList<Product> products = category.getProducts();
             for (Product product : products) {
